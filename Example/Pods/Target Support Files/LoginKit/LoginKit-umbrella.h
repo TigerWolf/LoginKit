546d6ed225da6baf62400c86d5ef5443798ef947
Kieran Andrews <kieran.andrews@adelaide.edu.au>
2016-02-14 14:33:21 +1030

    initial pod creation

---
+++ Example/Pods/Target Support Files/LoginKit/LoginKit-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double LoginKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char LoginKitVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-LoginKit_Tests/Pods-LoginKit_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_LoginKit_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LoginKit_TestsVersionString[];
+
