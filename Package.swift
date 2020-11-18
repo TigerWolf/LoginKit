e76c814eef0ef3dc2cf6fe52501393c535285331
George Nick Gorzynski <georgegorzynski@me.com>
2020-11-18 21:35:57 +0000

    Updated Package.swift
    SPM support now correctly implemented for minmum tools version.

--- Package.swift
+++ Package.swift
@@ -5,8 +5,33 @@ import PackageDescription
 
 let package = Package(
     name: "LoginKit",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "LoginKit",
+            targets: ["LoginKit"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", versions: Version(3, 2, 0)..<Version(4, 0, 0)),
-        .Package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git")
+        // Dependencies declare other packages that this package depends on.
+        .package(
+            url: "https://github.com/Alamofire/Alamofire.git",
+            .branch("master")
+        ),
+        .package(
+            url: "https://github.com/kishikawakatsumi/KeychainAccess.git",
+            .branch("master")
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "LoginKit",
+            dependencies: ["Alamofire", "KeychainAccess"],
+            path: "LoginKit"),
+        .testTarget(
+            name: "LoginKitTests",
+            dependencies: ["LoginKit","Alamofire", "KeychainAccess"],
+            path: "LoginKitTests"),
     ]
 )
