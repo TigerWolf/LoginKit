acb0e09aa2187f563b065dd758990f2f403fdec2
George Nick Gorzynski <31825917+g30r93g@users.noreply.github.com>
2020-11-18 21:17:55 +0000

    Update Swift-Tools to supported version

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "LoginKit",
     dependencies: [
         .Package(url: "https://github.com/Alamofire/Alamofire.git", versions: Version(3, 2, 0)..<Version(4, 0, 0)),
-        .Package(url: "https://github.com/kishikawakatsumi/KeychainAccess")
+        .Package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git")
     ]
 )
